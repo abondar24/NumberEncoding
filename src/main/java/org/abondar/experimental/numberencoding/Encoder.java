a89e2adf273c1dcf1dbf8180f0089d57475b1425
abondar24 <abondar1992@gmail.com>
2017-08-23 15:07:25 +0200

    Dictionary coding

--- src/main/java/org/abondar/experimental/numberencoding/Encoder.java
+++ src/main/java/org/abondar/experimental/numberencoding/Encoder.java
@@ -1,22 +1,19 @@
 package org.abondar.experimental.numberencoding;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
-* The class responsible for actual encoding
-* */
+ * The class responsible for actual encoding
+ */
 public class Encoder {
 
     private HashMap<Integer, String> codes;
 
     private List<String> numbers;
     private List<String> dictionary;
+    private HashMap<String,String> encodedDictionary;
+    private int minCodeLen;
 
     public Encoder(List<String> numbers, List<String> dictionary) {
         codes = new HashMap<>();
@@ -33,6 +30,7 @@ public class Encoder {
 
         this.numbers = numbers;
         this.dictionary = dictionary;
+        this.encodedDictionary = encodeDictionary(this.dictionary);
 
     }
 
@@ -45,7 +43,7 @@ public class Encoder {
         List<String> results = new ArrayList<>();
         numbers.forEach(num -> {
             if (checkNumberFormat(num)) {
-                String code = getCodedNumber(num);
+                getCodedNumbers(num);
             }
         });
 
@@ -54,7 +52,37 @@ public class Encoder {
 
 
     /**
+     * Code cities from dictionary into numbers
      * */
+    private HashMap<String,String> encodeDictionary(List<String>dictionary){
+        HashMap<String,String> result = new HashMap<>();
+
+        for (String str:dictionary){
+            StringBuilder cityCode = new StringBuilder();
+            for (Character c: str.toLowerCase().toCharArray()){
+                codes.forEach((key, value) -> {
+                    if (codeCheck(c, value)) {
+                        cityCode.append(key);
+                    }
+                });
+            }
+
+            result.put(cityCode.toString(),str);
+        }
+
+        result.forEach((k,v)->{
+            System.out.println(k +":"+v);
+        });
+
+        return result;
+    }
+
+
+    /**
+     * According to the task
+     * if number len is <3 - it's incorrect
+     * if number len is >6 and no - or / - it's incorrect
+     */
     private boolean checkNumberFormat(String number) {
 
         if (number.length() <= 3) {
@@ -70,11 +98,12 @@ public class Encoder {
             return number.contains("/") || number.contains("-");
         }
 
+
         return false;
     }
 
 
-    private String getCodedNumber(String number) {
+    private void getCodedNumbers(String number){
         System.out.println(number);
 
         if (codeCheck('-', number)) {
@@ -85,36 +114,23 @@ public class Encoder {
             number = number.replace("/", "");
         }
 
-
-        int [] intNum = parseNumberToArray(number);
-
-
-        List<String> locations = dictionary.stream().filter(val -> codeCheck(val.toLowerCase().charAt(0), codes.get(intNum[0])))
-                .filter(loc -> codeCheck(loc.toLowerCase().charAt(1), codes.get(intNum[1])))
-                .collect(Collectors.toList());
-
-      //  for (int  i=2;i<intNum.length;i++){
-      //      final int index = i;
-       //     locations = locations.stream()
-        //            .filter(loc-> loc.length()<=intNum.length)
-          //          .filter(loc -> codeCheck(loc.toLowerCase().charAt(index), codes.get(intNum[index])))
-            //        .collect(Collectors.toList());
-       // }
-
-        locations.forEach(System.out::println);
+        final String num = number;
 
 
-        return "";
+        encodedDictionary.forEach((key,val)->{
+
+        });
     }
+
+
 
     /**
      * helper method for checking if a string contains a char
-     **/
+     */
     private boolean codeCheck(char chr, String str) {
         return str.indexOf(chr) != -1;
 
     }
-
 
     private int[] parseNumberToArray(String number) {
         int[] intNum = new int[number.length()];
