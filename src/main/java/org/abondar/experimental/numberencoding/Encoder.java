edcbfb948d4287f951296e9a8ac9fa878aa124b5
abondar24 <abondar1992@gmail.com>
2017-08-26 01:30:55 +0200

    Removed redundant stuff

--- src/main/java/org/abondar/experimental/numberencoding/Encoder.java
+++ src/main/java/org/abondar/experimental/numberencoding/Encoder.java
@@ -36,29 +36,13 @@ public class Encoder {
     }
 
 
-    public HashMap<Integer, String> getCodes() {
-        return codes;
-    }
-
     public void doEncoding() {
         numbers.forEach(num -> {
-            List<String> results = new ArrayList<>();
+
             if (checkNumberFormat(num)) {
-                getCodedNumbers(num);
+                getCodedNumbers(num).forEach(System.out::println);
             }
-            results.forEach(System.out::println);
         });
-
-
-    }
-
-    /**
-     * Returns min length of city and code
-     */
-    private int calcMinCodeLen(List<String> dictionary) {
-        List<Integer> len = dictionary.stream().map(String::length).collect(Collectors.toList());
-
-        return Collections.min(len);
     }
 
 
@@ -85,21 +69,9 @@ public class Encoder {
     }
 
 
-    /**
-     * According to the task
-     * if number len is <3 - it's incorrect
-     * if number len is >6 and no - or / - it's incorrect
-     */
-    private boolean checkNumberFormat(String number) {
-
-        return number.length() > 3 && (number.length() < 6 || number.contains("/") || number.contains("-"));
-
-
-    }
-
-
     public List<String> getCodedNumbers(String number) {
         String cleanNumber = number;
+
         if (codeCheck('-', cleanNumber)) {
             cleanNumber = cleanNumber.replace("-", "");
         }
@@ -108,136 +80,68 @@ public class Encoder {
             cleanNumber = cleanNumber.replace("/", "");
         }
 
-
-        List<Combo> combos = getNumberCombos(cleanNumber);
-
-        combos.forEach(this::getCities);
-
-        combos = combos.stream()
-                .filter(c -> !c.getCities().isEmpty())
-                .collect(Collectors.toList());
-
-        HashMap<Integer, List<String>> positionCity = getPositionsOfCities(combos);
-
-        return getCityLists(positionCity, cleanNumber, number);
-    }
-
-    /**
-     * Gets lists of cities combined for position 0
-     */
-    public List<String> getCityLists(HashMap<Integer, List<String>> positionCity, final String cleanNumber, String number) {
-        List<List<String>> tmpResults = new ArrayList<>();
-
-        positionCity.forEach((k, v) -> {
-            if (positionCity.containsKey(0)) {
-                if (k == 0) {
-                    v.forEach(val -> {
-                        List<String> numberStr = new ArrayList<>();
-                        numberStr.add(val);
-                        tmpResults.add(numberStr);
-                    });
+        Set<String> existingCombos = new HashSet<>();
+        getNumberCombos(cleanNumber).forEach(c -> {
+            encodedDictionary.forEach((k, v) -> {
+                if (v.equals(c)) {
+                    existingCombos.add(c);
                 }
-            } else {
-                List<String> numberStr = new ArrayList<>();
-                numberStr.add(cleanNumber.substring(0, 1));
-                tmpResults.add(numberStr);
-            }
+            });
         });
 
+        final String cn = cleanNumber;
+        List<List<String>> tmpResutls = new ArrayList<>();
 
-        tmpResults.forEach(tr -> {
-
-            if (tr.get(0).length() != cleanNumber.length()) {
-
-                positionCity.forEach((k, v) -> {
-                    if (k != 0) {
-                        tr.addAll(v);
-                    }
-
-                });
-            }
-        });
-
-        return createStrings(tmpResults, number, cleanNumber);
-
-    }
-
-    private List<String> createStrings(List<List<String>> tmpResults, final String number, final String cleanNumber) {
-
-        List<String> res = new ArrayList<>();
-
-
-        tmpResults = tmpResults.stream()
-                .filter(tr -> !tr.isEmpty())
-                .collect(Collectors.toList());
-
-
-        tmpResults.forEach(tr -> {
-            StringBuilder sb = new StringBuilder();
-            sb.append(number);
-            sb.append(": ");
-            sb.append(tr.get(0));
-
-            if (!codeCheck('"', tr.get(0))) {
-                if (cleanNumber.length() - tr.get(0).length() == 1) {
-                    sb.append(" ");
-                    sb.append(cleanNumber.substring(cleanNumber.length() - 1, cleanNumber.length()));
-                    res.add(sb.toString());
-                }
-            } else if (cleanNumber.length() - tr.get(0).length() - 1 == 1) {
-                sb.append(" ");
-                sb.append(cleanNumber.substring(cleanNumber.length() - 1, cleanNumber.length()));
-                res.add(sb.toString());
+        existingCombos.forEach(ec -> {
+            List<String> combos = new ArrayList<>();
+            if (cn.indexOf(ec) == 0) {
+                combos.add(ec);
             }
 
-            if (tr.size() > 1) {
-                int overallLen = tr.get(0).length();
-                for (int i = 1; i < tr.size(); i++) {
-                    if (!codeCheck('"', tr.get(i))) {
-                        overallLen += tr.get(i).length();
-                    } else {
-                        overallLen += tr.get(i).length() - 1;
-                    }
-                    if (encodedDictionary.containsKey(tr.get(i - 1))) {
-                        if (!encodedDictionary.get(tr.get(i - 1)).contains(tr.get(i))) {
-                            if (!codeCheck(tr.get(i).charAt(0), tr.get(i - 1))) {
-                                if (overallLen == cleanNumber.length()) {
-                                    sb.append(" ");
-                                    sb.append(tr.get(i));
-                                    sb.append(" ");
-                                    res.add(sb.toString());
-                                } else if (cleanNumber.length() - overallLen == 1) {
-                                    sb.append(" ");
-                                    sb.append(cleanNumber.substring(i, cleanNumber.length()));
-                                    sb.append(" ");
-                                    res.add(sb.toString());
-                                }
-                            }
-                        }
-                    }
-
-                }
+            if (!combos.isEmpty()){
+                tmpResutls.add(combos);
             }
 
         });
 
-        res.forEach(System.out::println);
 
-        return res;
+        existingCombos.forEach(ec -> {
+            tmpResutls.forEach(tr->{
+                if (cn.indexOf(ec) > 0 && !codeCheck(ec.charAt(0),tr.get(0))) {
+                                  tr.add(ec);
+                }
+            });
+        });
+
+      List <String> res = new ArrayList<>();
+
+      if (!tmpResutls.isEmpty()){
+          System.out.println(tmpResutls);
+          tmpResutls.forEach(tr->{
+              tr.forEach(r->{
+
+              });
+          });
+      }
+
+
+
+        return new ArrayList<>();
     }
+
 
     /**
      * Returns all possible code combinations for the number
      */
-    public List<Combo> getNumberCombos(String number) {
-        List<Combo> combos = new ArrayList<>();
+    public List<String> getNumberCombos(String number) {
+        List<String> combos = new ArrayList<>();
 
         int window = MIN_CODE_LEN;
 
         while (window < number.length() + 1) {
             for (int i = 0; i < number.length() - window + 1; i++) {
                 String combo = number.substring(i, i + window);
-                combos.add(new Combo(combo, i));
+                combos.add(combo);
             }
             window++;
         }
@@ -249,34 +153,36 @@ public class Encoder {
     /**
      * Fills each combo with corresponding cities
      */
-    public Combo getCities(Combo combo) {
-        List<String> cities = new ArrayList<>();
+     public List<String> getCitiesForCombo(String combo) {
+     List<String> cities = new ArrayList<>();
 
-        encodedDictionary.forEach((k, v) -> {
-            if (v.equals(combo.getCombo())) {
-                cities.add(k);
-            }
-        });
+     encodedDictionary.forEach((k, v) -> {
+     if (v.equals(combo)) {
+     cities.add(k);
+     }
+     });
 
-        combo.setCities(cities);
-        return combo;
+     return cities;
+     }
+
+
+    /**
+     * According to the task
+     * if number len is <3 - it's incorrect
+     * if number len is >6 and no - or / - it's incorrect
+     */
+    private boolean checkNumberFormat(String number) {
+        return number.length() >= 3 && (number.length() <= 6 || number.contains("/") || number.contains("-"));
     }
 
 
-    private HashMap<Integer, List<String>> getPositionsOfCities(List<Combo> combos) {
-        HashMap<Integer, List<String>> positionCity = new HashMap<>();
+    /**
+     * Returns min length of city and code
+     */
+    private int calcMinCodeLen(List<String> dictionary) {
+        List<Integer> len = dictionary.stream().map(String::length).collect(Collectors.toList());
 
-        combos.stream().sorted((cL, cR) -> cL.getPos().compareTo(cR.getPos()))
-                .forEach(c -> positionCity.put(c.getPos(), new ArrayList<>()));
-
-
-        combos.forEach(c -> positionCity.forEach((k, v) -> {
-            if (k == c.getPos()) {
-                v.addAll(c.getCities());
-            }
-        }));
-
-        return positionCity;
+        return Collections.min(len);
     }
 
     /**
